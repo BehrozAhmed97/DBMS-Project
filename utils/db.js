b301ad9f9f0a391c126ab7d2919501da03edaade
Behroz Ahmed <behroz.ahmed97@gmail.com>
2026-07-04 15:40:17 +0500

    Update database schemas and queries

--- utils/db.js
+++ utils/db.js
@@ -1,3 +1,3 @@
 import { neon } from '@neondatabase/serverless';
 
-export const SQL = neon('postgresql://neondb_owner:npg_mnt4dJMus3UX@ep-aged-sea-ai5er833-pooler.c-4.us-east-1.aws.neon.tech/academic?sslmode=require&channel_binding=require');
+export const SQL = neon('postgresql://neondb_owner:npg_mnt4dJMus3UX@ep-aged-sea-ai5er833-pooler.c-4.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require');
